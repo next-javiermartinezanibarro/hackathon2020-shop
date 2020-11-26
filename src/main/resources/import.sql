2a14efb4c76fc365a97edfb93a3f9a33d7e9da0f
Javier Martínez Aníbarro <javier.martinez.anibarro.next@bbva.com>
2020-11-26 19:26:05 +0100

    Data demo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,19 @@
 
 INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
-(nextval('hibernate_sequence'), 'pet', 'pet 1', 1, 200);
+(nextval('hibernate_sequence'), 'pet', 'Abeja trabajadora', 1, 10);
 INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
-(nextval('hibernate_sequence'), 'pet', 'pet 2', 3, 500);
+(nextval('hibernate_sequence'), 'pet', 'Pez saltarín', 1, 15);
 INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
-(nextval('hibernate_sequence'), 'background', 'background 1', 1, 300);
+(nextval('hibernate_sequence'), 'pet', 'Agente Rana', 2, 20);
+INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
+(nextval('hibernate_sequence'), 'pet', 'Conejo de la suerte', 2, 25);
+INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
+(nextval('hibernate_sequence'), 'pet', 'Perro fiel', 3, 30);
+INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
+(nextval('hibernate_sequence'), 'pet', 'Gato cariñoso', 3, 35);
+INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
+(nextval('hibernate_sequence'), 'background', 'Viaje al fondo del mar', 1, 30);
+INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
+(nextval('hibernate_sequence'), 'background', 'Vuela como un pájaro', 2, 50);
+INSERT INTO item (id, type, name, requiredlevel, requiredgems) VALUES
+(nextval('hibernate_sequence'), 'background', 'Explora el universo', 3, 70);
